73650468ffe0b2563274b75188d88cfd696004e8
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-09-01 14:08:24 -0500

    [chg] root path msg

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 var router = require('express').Router();
 
-router.get('/', (req, res)=>{
-  res.send('welcome to adoptapet api');
-})
+router.get('/', (req, res) => { res.send('Adoptapet API V1'); })
 router.use('/usuarios', require('./usuarios'));
 router.use('/solicitudes', require('./solicitudes'));
 router.use('/mascotas', require('./mascotas'));
-
 
 module.exports = router;
